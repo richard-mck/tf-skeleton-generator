ebd67cd286125b9720ed88fdf4ff019a80fef3a9
Richard McKenzie <richard@openoceanrobotics.com>
2024-06-05 11:47:22 -0700

    feat: add file skeletons as tuples for immutability

--- tf_skeleton.py
+++ tf_skeleton.py
@@ -1,4 +1,51 @@
 """A generator to create a Terraform project, reducing startup time"""
 
+BACKEND_TF = (
+    "backend.tf",
+    """
+terraform {
+  required_version = ">= 1.3"
+  required_providers {}
+  backend "http" {}
+  # Edit this to fit your environment
+}
+""",
+)
+
+MAIN_TF = (
+    "main.tf",
+    """
+resource "random_id" "id" {
+  byte_length = 8
+}
+""",
+)
+
+OUTPUTS_TF = (
+    "outputs.tf",
+    """
+output "required" {
+  description = "This prints the contents of a variable when Terraform is run"
+  value       = var.required_var
+}
+""",
+)
+
+VARIABLES_TF = (
+    "variables.tf",
+    """
+variable "required_var" {
+  type        = string
+  description = "This is a required variable"
+}
+
+variable "optional_var" {
+  type        = string
+  description = "This is an optional variable"
+  default     = "Optional"
+}
+""",
+)
+
 if __name__ == "__main__":
     pass
