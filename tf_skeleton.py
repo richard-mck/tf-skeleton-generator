6cdaeb4b8fff562d5c1568cfb2b693573fed651c
Richard McKenzie <richard@openoceanrobotics.com>
2024-06-05 13:54:30 -0700

    refactor: create class to handle generator operations

--- tf_skeleton.py
+++ tf_skeleton.py
@@ -51,20 +51,28 @@ variable "optional_var" {
 )
 
 
+class SkeletonGenerator:
+    def __init__(self, project_name):
+        self.project_name = project_name
+
+    def setup_project_working_directory(self):
+        os.mkdir(self.project_name)
+        os.chdir(self.project_name)
+
+    def generate_project(self):
+        self.setup_project_working_directory()
+        tf_files = [BACKEND_TF, MAIN_TF, OUTPUTS_TF, VARIABLES_TF]
+        for title in tf_files:
+            print(title)
+            write_content_to_file(title[0], title[1])
+
+
 def write_content_to_file(file_name: str, content: str):
     with open(file_name, "w") as f:
         f.write(content)
 
 
-def setup_project_working_directory(project_name: str):
-    os.mkdir(project_name)
-    os.chdir(project_name)
-
-
 if __name__ == "__main__":
     project_name = sys.argv[1]
-    setup_project_working_directory(project_name)
-    tf_files = [BACKEND_TF, MAIN_TF, OUTPUTS_TF, VARIABLES_TF]
-    for title in tf_files:
-        print(title)
-        write_content_to_file(title[0], title[1])
+    tf_gen = SkeletonGenerator(project_name)
+    tf_gen.generate_project()
