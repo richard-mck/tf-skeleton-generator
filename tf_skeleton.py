fec02ae4b2b777e7b323362062dc7c4737d15d1c
Richard McKenzie <richard@openoceanrobotics.com>
2024-06-05 15:50:13 -0700

    refactor: split file generation into own function to make roles clearer

--- tf_skeleton.py
+++ tf_skeleton.py
@@ -57,17 +57,20 @@ variable "optional_var" {
 class SkeletonGenerator:
     def __init__(self, project_name):
         self.project_name = project_name
+        self.file_list = [BACKEND_TF, MAIN_TF, OUTPUTS_TF, VARIABLES_TF]
 
     def setup_project_working_directory(self):
         os.mkdir(self.project_name)
         os.chdir(self.project_name)
 
+    def setup_tf_files(self):
+        for title in self.file_list:
+            print(title)
+            write_content_to_file(title.file_name, title.file_content)
+
     def generate_project(self):
         self.setup_project_working_directory()
-        tf_files = [BACKEND_TF, MAIN_TF, OUTPUTS_TF, VARIABLES_TF]
-        for title in tf_files:
-            print(title)
-            write_content_to_file(title[0], title[1])
+        self.setup_tf_files()
 
 
 def write_content_to_file(file_name: str, content: str):
